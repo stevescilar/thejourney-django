fb76808fefe2b5a2963d5b7c5be94d3962a574be
stevescilar <stevemuambi@gmail.com>
2023-06-07 17:46:58 +0300

    first step

---
+++ blog/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.utils import timezone
+# import for creating M-1 relationship
+from django.contrib.auth.models import User
+
+# Model manager for getting published post
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+
+    # blogs require statuses(published/draft)
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    # Link a user with a post - many to one relationship
+    author = models.ForeignKey(User,on_delete = models.CASCADE,
+                                related_name = 'blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                                choices = Status.choices,
+                                default= Status.DRAFT)
+
+    
+    objects = models.Manager() #the default manager
+    published = PublishedManager() #custom manager
+    # ordering the post from newest to oldest
+    class Meta:
+        ordering = ['-publish']
+        # adding index to improve on search performances
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+
+
+    def __str__(self):
+        return self.title
